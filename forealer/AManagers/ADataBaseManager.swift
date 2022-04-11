a6ea8afbcfd3debd7c1b5681bc39c85fed6c48e9
Misha Causur <mishacausur@mail.ru>
2022-04-11 23:31:18 +0500

    protocol implementation is done

---
+++ forealer/AManagers/ADataBaseManager.swift
@@ -0,0 +1,60 @@
+//
+//  ADataBaseManager.swift
+//  forealer
+//
+//  Created by Misha Causur on 11.04.2022.
+//
+
+import RealmSwift
+import Foundation
+import Combine
+
+public struct AnyScheduler<SchedulerTimeType, SchedulerOptions>: Scheduler where SchedulerTimeType: Strideable, SchedulerTimeType.Stride: SchedulerTimeIntervalConvertible {
+    public var now: SchedulerTimeType
+    
+    public func schedule(options: SchedulerOptions?, _ action: @escaping () -> Void) { }
+    
+    public func schedule(after date: SchedulerTimeType, interval: SchedulerTimeType.Stride, tolerance: SchedulerTimeType.Stride, options: SchedulerOptions?, _ action: @escaping () -> Void) -> Cancellable { }
+    
+    public func schedule(after date: SchedulerTimeType, tolerance: SchedulerTimeType.Stride, options: SchedulerOptions?, _ action: @escaping () -> Void) { }
+    
+    public var minimumTolerance: SchedulerTimeType.Stride
+}
+
+public typealias AnySchedulerOf<Scheduler> = AnyScheduler<Scheduler.SchedulerTimeType, Scheduler.SchedulerOptions> where Scheduler: Combine.Scheduler
+
+final class ADataBaseManager: ADataBaseManagerProtocol {
+   
+    private let configuration: Realm.Configuration
+    private let regularScheduler: AnySchedulerOf<DispatchQueue>
+    private let listenScheduler: AnySchedulerOf<RunLoop>
+    
+    init(configuration: Realm.Configuration,
+         regularScheduler: AnySchedulerOf<DispatchQueue>,
+         listenScheduler: AnySchedulerOf<RunLoop>) {
+        self.regularScheduler = regularScheduler
+        self.listenScheduler = listenScheduler
+        self.configuration = configuration
+    }
+    
+    func realm<S: Scheduler>(scheduler: S) -> AnyPublisher<Realm, Error> {
+        return Just((configuration, nil))
+    }
+    
+    func getData<DataBase: Object, Entity>(mapper: @escaping DataBaseToModelMapper<DataBase, Entity>, block: @escaping RealmBlock<DataBase>) -> AnyPublisher<Entity?, Error> {
+        
+    }
+    
+    func listen<DataBase: Object, Entity>(mapper: @escaping DataBaseToModelMapper<DataBase, Entity>, block: @escaping RealmBlock<DataBase>) -> AnyPublisher<Entity, Error> {
+        
+    }
+    
+    func save<DataBase: Object, Entity>(object: Entity, mapper: @escaping DataBaseModelMapper<DataBase, Entity>, update: Realm.UpdatePolicy) -> AnyPublisher<Void, Error> {
+        
+    }
+    
+    func delete<DataBase: Object>(_ type: DataBase.Type, block: @escaping RealmBlock<DataBase>) -> AnyPublisher<Void, Error> {
+        
+    }
+    
+}
