69b4e5e4cee85e2aaa6582de39d87fa3f53ff667
Misha Causur <mishacausur@mail.ru>
2022-05-04 19:36:43 +0500

    collection SecondCell is implemented

--- forealer/AManagers/ADataBaseManager.swift
+++ forealer/AManagers/ADataBaseManager.swift
@@ -9,49 +9,49 @@ import RealmSwift
 import Foundation
 import Combine
 
-//public typealias AnySchedulerOf<Scheduler> = AnyScheduler<Scheduler.SchedulerTimeType, Scheduler.SchedulerOptions> where Scheduler: Combine.Scheduler
-//
-//final class ADataBaseManager: ADataBaseManagerProtocol {
-//   
-//    private let configuration: Realm.Configuration
-//    private let regularScheduler: AnySchedulerOf<DispatchQueue>
-//    private let listenScheduler: AnySchedulerOf<RunLoop>
-//    
-//    init(configuration: Realm.Configuration,
-//         regularScheduler: AnySchedulerOf<DispatchQueue>,
-//         listenScheduler: AnySchedulerOf<RunLoop>) {
-//        self.regularScheduler = regularScheduler
-//        self.listenScheduler = listenScheduler
-//        self.configuration = configuration
-//    }
-//    
-//    func realm<S: Scheduler>(scheduler: S) -> AnyPublisher<Realm, Error> {
-//        return Just((configuration, nil))
-//            .receive(on: scheduler)
-//            .tryMap(Realm.init)
+public typealias AnySchedulerOf<Scheduler> = AnyScheduler<Scheduler.SchedulerTimeType, Scheduler.SchedulerOptions> where Scheduler: Combine.Scheduler
+
+final class ADataBaseManager: ADataBaseManagerProtocol {
+   
+    private let configuration: Realm.Configuration
+    private let regularScheduler: AnySchedulerOf<DispatchQueue>
+    private let listenScheduler: AnySchedulerOf<RunLoop>
+    
+    init(configuration: Realm.Configuration,
+         regularScheduler: AnySchedulerOf<DispatchQueue>,
+         listenScheduler: AnySchedulerOf<RunLoop>) {
+        self.regularScheduler = regularScheduler
+        self.listenScheduler = listenScheduler
+        self.configuration = configuration
+    }
+    
+    func realm<S: Scheduler>(scheduler: S) -> AnyPublisher<Realm, Error> {
+        return Just((configuration, nil))
+            .receive(on: scheduler)
+            .tryMap(Realm.init)
+            .eraseToAnyPublisher()
+    }
+    
+//    func getData<DataBase: Object, Entity>(mapper: @escaping DataBaseToModelMapper<DataBase, Entity>, block: @escaping RealmBlock<DataBase>) -> AnyPublisher<Entity?, Error> {
+//        return realm(scheduler: regularScheduler)
+//            .map { $0.objects(DataBase.self) }
+//            .map({ result in
+//                block(result)
+//            })
+////            .map { block($0).last.map(mapper) }
 //            .eraseToAnyPublisher()
 //    }
-//    
-////    func getData<DataBase: Object, Entity>(mapper: @escaping DataBaseToModelMapper<DataBase, Entity>, block: @escaping RealmBlock<DataBase>) -> AnyPublisher<Entity?, Error> {
-////        return realm(scheduler: regularScheduler)
-////            .map { $0.objects(DataBase.self) }
-////            .map({ result in
-////                block(result)
-////            })
-//////            .map { block($0).last.map(mapper) }
-////            .eraseToAnyPublisher()
-////    }
-//    
-//    func listen<DataBase: Object, Entity>(mapper: @escaping DataBaseToModelMapper<DataBase, Entity>, block: @escaping RealmBlock<DataBase>) -> AnyPublisher<Entity, Error> {
-//        
-//    }
-//    
-//    func save<DataBase: Object, Entity>(object: Entity, mapper: @escaping DataBaseModelMapper<DataBase, Entity>, update: Realm.UpdatePolicy) -> AnyPublisher<Void, Error> {
-//        
-//    }
-//    
-//    func delete<DataBase: Object>(_ type: DataBase.Type, block: @escaping RealmBlock<DataBase>) -> AnyPublisher<Void, Error> {
-//        
-//    }
-//    
-//}
+    
+    func listen<DataBase: Object, Entity>(mapper: @escaping DataBaseToModelMapper<DataBase, Entity>, block: @escaping RealmBlock<DataBase>) -> AnyPublisher<Entity, Error> {
+        
+    }
+    
+    func save<DataBase: Object, Entity>(object: Entity, mapper: @escaping DataBaseModelMapper<DataBase, Entity>, update: Realm.UpdatePolicy) -> AnyPublisher<Void, Error> {
+        
+    }
+    
+    func delete<DataBase: Object>(_ type: DataBase.Type, block: @escaping RealmBlock<DataBase>) -> AnyPublisher<Void, Error> {
+        
+    }
+    
+}
--- forealer/Flows/SecondCell.m
+++ forealer/Flows/SecondCell.m
@@ -9,4 +9,25 @@
 
 @implementation SecondCell
 
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (id)init {
+    if (self = [super initWithTitle:LocStr(@"CONTAINER_LIST_TITLE") navBarHidden:NO]) {
+        m_paths = [ContainerList shared].paths;
+
+        [[NSNotificationCenter defaultCenter] addObserver:self
+            selector:@selector(onContainerListDidChange)
+            name:kSDKLauncherContainerListDidChange object:nil];
+    }
+
+    return self;
+}
+
+- (void)loadView {
+    self.view = [[UIView alloc] init];
+    UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
+}
+
 @end
